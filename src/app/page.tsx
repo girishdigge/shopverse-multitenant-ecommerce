7dc07082e6a2e8981a42070121327d1b1678f66d
girishdigge <girishdigge@gmail.com>
2025-04-25 21:35:14 +0530

    shadcn component costumization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Progress } from '@/components/ui/progress';
+import { Textarea } from '@/components/ui/textarea';
+
 export default function Home() {
-  return <div>Shop Verse</div>;
+  return (
+    <div className='p-4'>
+      <div className='flex flex-col gap-y-4'>
+        <div>
+          <Button variant='elevated' className='text-3xl'>
+            Shop Verse
+          </Button>
+        </div>
+        <div>
+          <Input placeholder='Input' />
+        </div>
+        <div>
+          <Progress value={50} />
+        </div>
+        <div>
+          <Textarea placeholder='textarea' />
+        </div>
+      </div>
+    </div>
+  );
 }
