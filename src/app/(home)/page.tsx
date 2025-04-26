5d458fa19b89ee029a5a5f1d0d7f766a43bda10c
girishdigge <girishdigge@gmail.com>
2025-04-26 12:04:34 +0530

    home layout

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button';
+import { Checkbox } from '@/components/ui/checkbox';
 import { Input } from '@/components/ui/input';
 import { Progress } from '@/components/ui/progress';
 import { Textarea } from '@/components/ui/textarea';
